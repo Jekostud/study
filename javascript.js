aebe1098e13a1f585c042b05b03d4b950082e072
jekostud <tillerman9@gmail.com>
2021-12-05 15:41:34 +0300

    feat: function solutions

--- javascript.js
+++ javascript.js
@@ -3,3 +3,32 @@ function circSquare() {
     let s = ((a*a)/4) * Math.PI;
     return document.form1.res.value = s.toFixed( 2 );
 }
+function circumference() {
+    let a = document.form2.num2.value;
+    let s = a * Math.PI;
+    return document.form2.res.value = s.toFixed( 2 );
+}
+
+function eurUsd() {
+    let a = document.form3.num3.value;
+    let s = a / 1.1;
+    return document.form3.res.value = s.toFixed( 2 );
+}
+function blrUkr() {
+    let a = document.form4.num1.value;
+    let b = document.form4.num2.value;
+    let c = a / b;
+    return document.form4.res.value = c.toFixed( 2 );
+}
+function mowBer() {
+    let a = document.form5.num1.value;
+    let b = document.form5.num2.value;
+    let c = a / b;
+    return document.form5.res.value = c.toFixed( 2 );
+}
+
+/* 207 600 - Belarus
+603 628 - Ukraine
+2511 MOW
+891.8 BER
+ */
